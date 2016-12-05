97a4945516fe74ffe654e0d1c5562f90bd37d06f
NikolajHolm <niko4467@edu.campusvejle.dk>
2016-12-05 10:27:58 +0100

    Tilføjede index-php og slettede index.html da den ikke var nødvendig

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cats</title>
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Google Fonts Merriweather -->
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+</head>
+<body>
+	<header></header>
+	<nav></nav>
+	<main></main>
+	<footer></footer>
+</body>
+</html>
