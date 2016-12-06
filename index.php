2bc40930fb3527c1943a5858bfb8a7dbd6326667
NikolajHolm <niko4467@edu.campusvejle.dk>
2016-12-06 09:08:53 +0100

    Næsten færdig med design

--- index.php
+++ index.php
@@ -4,15 +4,40 @@
 	<meta charset="UTF-8">
 	<title>Cats</title>
 	<!-- CSS -->
-	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
 	<!-- Google Fonts Merriweather -->
 	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
 </head>
 <body>
-	<header></header>
-	<nav></nav>
-	<main></main>
-	<footer></footer>
-</body>
-</html>
+	<div class="container-fluid noSidePadding">
+		<header class="">
+			<img src="images/header.jpg" alt="" class="img-responsive  center-block ">
+		</header>
+		<div class="row">
+			<nav class="navbar navbar-inverse ">
+				<ul class="nav navbar-nav ">
+					<li class=""><a href="#">Forside</a></li>
+					<li><a href="#">Galleri</a></li>
+				</ul>
+				<ul class="nav navbar-nav text-right">
+					<li><label for="usermail">Email</label>
+						<input type="email" name="usermail" placeholder="Username" required></li>
+						<li><label for="password">Password</label>
+							<input type="password" name="password" placeholder="Password" required></li>
+							<li>
+								<input class="login" type="submit" value="Login"></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<main class="col-md-12 row">
+					<article class="col-md-12">
+						<img src="images/cat1.jpg" class="center-block" alt="">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</article>
+				<footer>
+
+				</footer>
+			</body>
+			</html>
